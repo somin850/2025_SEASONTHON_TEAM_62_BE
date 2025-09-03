f96008fba1e177fdac90960d3e287aff338b2f3a
SEUNGHYEOKNOH <109861378+SEUNGHYEOKNOH@users.noreply.github.com>
2025-09-03 20:48:25 +0900

    feat(global) - excepptionhandler,BaseEntity

---
+++ src/main/java/com/kbsw/seasonthon/global/base/response/exception/ExceptionType.java
@@ -0,0 +1,37 @@
+package com.kbsw.seasonthon.global.base.response.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum ExceptionType {
+    // common
+    UNEXPECTED_SERVER_ERROR(INTERNAL_SERVER_ERROR, "C001", "예상치못한 서버에러 발생"),
+    BINDING_ERROR(BAD_REQUEST, "C002", "바인딩시 에러 발생"),
+    ESSENTIAL_FIELD_MISSING_ERROR(NO_CONTENT , "C003","필수적인 필드 부재"),
+    INVALID_VALUE_ERROR(NOT_ACCEPTABLE , "C004","값이 유효하지 않음"),
+    DUPLICATE_VALUE_ERROR(NOT_ACCEPTABLE , "C005","값이 중복됨"),
+
+    // auth
+    INVALID_REFRESH_TOKEN(NOT_ACCEPTABLE , "A001","유효하지 않은 리프레시 토큰"),
+    REFRESH_TOKEN_EXPIRED(UNAUTHORIZED,"A002","리프레시 토큰 만료"),
+    PASSWORD_NOT_MATCHED(NOT_ACCEPTABLE , "A003","비밀번호 불일치"),
+
+    // oauth2
+    INVALID_PROVIDER_TYPE_ERROR(NOT_ACCEPTABLE , "O001","지원하지 않는 provider"),
+
+    // user
+    USER_NOT_FOUND(NOT_FOUND, "U001", "존재하지 않는 사용자"),
+    DUPLICATED_USER_ID(CONFLICT, "U002", "중복 아이디(PK)"),
+    DUPLICATED_USERNAME(CONFLICT, "U003", "중복 아이디(username)"),
+    ALREADY_REGISTERED_USER(NOT_ACCEPTABLE , "U006","이미 최종 회원 가입된 사용자"),
+    NOT_REGISTERED_USER(FORBIDDEN , "U007","최종 회원 가입 되지 않은 사용자"),
+    UNAUTHORIZED_USER(UNAUTHORIZED, "U005","로그인 되지 않은 사용자");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;}
